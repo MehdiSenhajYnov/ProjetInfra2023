3249da21025deeaa27bf34110cc5d6eea9c0b83f
MehdiSenhajYnov <93327385+MehdiSenhajYnov@users.noreply.github.com>
2023-02-24 17:20:14 +0100

    Connect Marche, pas de crash

--- UnityProject/Assets/NetworkManager.cs
+++ UnityProject/Assets/NetworkManager.cs
@@ -38,7 +38,7 @@ namespace GameNetClient
         // Start is called before the first frame update
         void Start()
         {
-            Debug.Log("Debut");
+            tokenSource = new CancellationTokenSource();
             int counter = 1;
             foreach (ActionCodes oneAction in Enum.GetValues(typeof(ActionCodes)))
             {
@@ -46,18 +46,23 @@ namespace GameNetClient
                 counter++;
             }
             Actions[ActionCodes.GetOtherName] = 48;
-            Debug.Log("Milieu");
 
             showScreen(0);
-            Debug.Log("ShowScreen");
 
-            //ConnectBtn.onClick.AddListener(ConnectToServer);
+
+            ConnectBtn.onClick.AddListener(ConnectToServer);
             LoginBtn.onClick.AddListener(SendLoginPacket);
-            Debug.Log("Listeners");
+
+
 
             Utilities.DesactivateDebugger();
-            Debug.Log("Fini");
+            Task.Run(() =>
+            {
+                ReceiveResponse();
+            }, tokenSource.Token);
         }
+
+
 
         public async void ConnectToServer()
         {
@@ -66,26 +71,31 @@ namespace GameNetClient
             var address = IPAddress.Parse(ip);
             Debug.Log("Connection attempt to " + ip + ":" + Port + "...");
 
-            try
+            await Task.Run(() =>
             {
-                await Task.Run(() => clientSocket.ConnectAsync(address, Port), tokenSource.Token);
-            }
-            catch (SocketException e)
-            {
-                Debug.Log("Connexion Echoué!");
-                Debug.Log(e.Message);
-                return;
-            }
+                try
+                {
+                    clientSocket.Connect(address, Port);
+                }
+                catch (SocketException e)
+                {
+                    Debug.Log("Connexion Echoué!");
+                    Debug.Log(e.Message);
+                    return;
+                }
+            }, tokenSource.Token);
+
             showScreen(1);
             
             Utilities.Debugger("Connected!");
+            Debug.Log("Connected");
         }
 
         public void SendLoginPacket()
         {
             if (userNameInput.text == "")
             {
-                Utilities.Debugger("Choisi un Nom Valide !");
+                Utilities.Debugger("Choisis un nom valide !");
                 return;
             }
             if (!hasLoggedin)
@@ -98,79 +108,147 @@ namespace GameNetClient
 
         void Update()
         {
-            ReceiveResponse();
+            //ReceiveResponse();
         }
 
         public void ReceiveResponse()
         {
-            if (clientSocket == null || !clientSocket.Connected) return;
-            var buffer = new byte[2048];
-            int received = clientSocket.Receive(buffer, SocketFlags.None);
-            if (received == 0)
+            while(true)
             {
-                return;
-            }
+                if (clientSocket == null || !clientSocket.Connected) continue;
+                var buffer = new byte[2048];
+                int received = clientSocket.Receive(buffer, SocketFlags.None);
+                if (received == 0)
+                {
+                    continue;
+                }
 
-            var data = new byte[received];
-            Array.Copy(buffer, data, received);
+                var data = new byte[received];
+                Array.Copy(buffer, data, received);
 
             
-            // Nom du joueur adveresaire
-            if (data[0] == 48 && data.Length > 1)
-            {
-                string text = Encoding.ASCII.GetString(data.Skip(1).Take(data.Length - 1).ToArray());
-                if (game.plyrOneName == "")
+                // Nom du joueur adveresaire
+                if (data[0] == 48 && data.Length > 1)
                 {
-                    game.plyrOneName = text;
-                    Utilities.Debugger("Your enemy is : " + game.plyrOneName);
+                    string text = Encoding.ASCII.GetString(data.Skip(1).Take(data.Length - 1).ToArray());
+                    if (game.plyrOneName == "")
+                    {
+                        game.plyrOneName = text;
+                        Utilities.Debugger("Your enemy is : " + game.plyrOneName);
+                    }
+                    else if (game.plyrTwoName == "")
+                    {
+                        game.plyrTwoName = text;
+                        Utilities.Debugger("Your enemy is : " + game.plyrTwoName);
+                    }
+                    continue;
                 }
-                else if (game.plyrTwoName == "")
+
+                /*
+                if (data[0] == 5)
                 {
-                    game.plyrTwoName = text;
-                    Utilities.Debugger("Your enemy is : " + game.plyrTwoName);
+                    Game.UpdatePlyrsFromServer(data.Skip(1).ToArray());
+                    int PlyrInput = Game.PlyrGetAtkInput((byte)(myID == 1 ? data[1] : data[5]));
+                    int CiblePlyr = Game.CibleOfAtkInput();
+                    SendByte(new byte[] { 4, (byte)PlyrInput, (byte)CiblePlyr });
+
+                    return;
                 }
-                return;
-            }
 
-            /*
-            if (data[0] == 5)
-            {
-                Game.UpdatePlyrsFromServer(data.Skip(1).ToArray());
-                int PlyrInput = Game.PlyrGetAtkInput((byte)(myID == 1 ? data[1] : data[5]));
-                int CiblePlyr = Game.CibleOfAtkInput();
-                SendByte(new byte[] { 4, (byte)PlyrInput, (byte)CiblePlyr });
-
-                return;
-            }
-
-            if (data[0] == 6)
-            {
-                Game.UpdatePlyrsFromServer(data.Skip(1).ToArray());
-                Console.Write("\n");
-                Console.WriteLine(Game.PlayerOne.ToString());
-                Console.WriteLine(Game.PlayerTwo.ToString());
-                Console.WriteLine("Waiting for " + (myID == 1 ? Game.plyrTwoName : Game.plyrOneName) + " play ...");
-                return;
-            }
-
-            // ActionsQueue
-            if (ActionsQueue.Count == 0)
-            {
-                foreach (var oneByte in data)
+                if (data[0] == 6)
                 {
-                    ActionsQueue.Add(oneByte);
+                    Game.UpdatePlyrsFromServer(data.Skip(1).ToArray());
+                    Console.Write("\n");
+                    Console.WriteLine(Game.PlayerOne.ToString());
+                    Console.WriteLine(Game.PlayerTwo.ToString());
+                    Console.WriteLine("Waiting for " + (myID == 1 ? Game.plyrTwoName : Game.plyrOneName) + " play ...");
+                    return;
                 }
-                PlayForVariable();
-            }
-            else
-            {
-                foreach (var oneByte in data)
+                 */
+
+                // ActionsQueue
+                if (ActionsQueue.Count == 0)
                 {
-                    ActionsQueue.Add(oneByte);
+                    foreach (var oneByte in data)
+                    {
+                        ActionsQueue.Add(oneByte);
+                    }
+                    PlayForVariable();
                 }
-            }*/
+                else
+                {
+                    foreach (var oneByte in data)
+                    {
+                        ActionsQueue.Add(oneByte);
+                    }
+                }
+
+            }
         }
 
+        public void PlayForVariable()
+        {
+            while (true)
+            {
+                if (ActionsQueue.Count == 0) return;
+                DoActionFromByte(ActionsQueue[0]);
+                ActionsQueue.RemoveAt(0);
+            }
+        }
+
+        public void DoActionFromByte(byte oneByte)
+        {
+            if (oneByte == Actions[ActionCodes.SetIdToOne])
+            {
+                myID = 1;
+            }
+            if (oneByte == Actions[ActionCodes.SetIdToTwo])
+            {
+                myID = 2;
+            }
+            if (oneByte == Actions[ActionCodes.SetplyrName])
+            {
+                game.SetName(myID);
+
+
+                // Send Name to server
+                byte[] PlyrNameByte = Encoding.ASCII.GetBytes(((byte)0).ToString() + myID.ToString() + (myID == 1 ? game.plyrOneName : game.plyrTwoName));
+                clientSocket.Send(PlyrNameByte);
+                //Console.WriteLine("Sended Name To Server");
+            }
+            if (oneByte == Actions[ActionCodes.PlyrChoice])
+            {
+                byte PlyrChosen = game.PlyrChoice(myID == 1 ? game.plyrOneName : game.plyrTwoName);
+                SendByte(new byte[] { PlyrChosen, (byte)myID });
+            }
+            if (oneByte == Actions[ActionCodes.PlyrOneWinner])
+            {
+                Debug.Log(game.plyrOneName + " WIN!");
+                Exit();
+            }
+            if (oneByte == Actions[ActionCodes.PlyrTwoWinner])
+            {
+                Debug.Log(game.plyrTwoName + " WIN!");
+                Exit();
+            }
+            if (oneByte == Actions[ActionCodes.DrawMatch])
+            {
+                Debug.Log("DRAW!");
+                Exit();
+            }
+        }
+
+
+        public void Exit()
+        {
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+
+        private void SendByte(byte[] byby)
+        {
+            clientSocket.Send(byby);
+        }
 
         public void SendString(string text)
         {
@@ -213,6 +291,12 @@ namespace GameNetClient
         private void OnDisable()
         {
             tokenSource.Cancel();
+            if (clientSocket.Connected)
+            {
+                clientSocket.Disconnect(false);
+            }
+            clientSocket.Close();
+
         }
     }
 }
